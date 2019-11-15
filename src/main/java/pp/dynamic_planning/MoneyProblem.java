c74de1cba34d415720e5a850050bb63a12adef3b
lijingxian <lijingxian@cabrtech.com>
2019-11-15 10:07:35 +0800

    money problem

--- src/main/java/pp/dynamic_planning/MoneyProblem.java
+++ src/main/java/pp/dynamic_planning/MoneyProblem.java
@@ -6,16 +6,17 @@ package pp.dynamic_planning;
  * 遍历每个值时，都有两种可能，取或不取
  */
 public class MoneyProblem {
-
+    private MoneyProblem() {
+    }
     // 从第一个数字开始遍历，sum初始为0
 
-    private static boolean money1(int[] arr, int aim) {
+    static boolean money1(int[] arr, int aim) {
         return process(arr, 0, 0, aim);
     }
 
     /**
      * 递归
-     *
+     * 正推, 计算能否到达base case的位置
      * @param i   第i个位置
      * @param sum 到第i个位置时的累加和,i-1后的累加和
      */
@@ -32,21 +33,23 @@ public class MoneyProblem {
 
     /**
      * 动态递归
-     * 可变参数是i和sum, 二维表, 0~
-     *
-     * @return
+     * 可变参数是i和sum, 二维表, 0~arr.length  0~aim
+     * 从后向前计算,(i,j)位置是否为true, 取决于sum 加/不加arr[i] == aim, 列j表示当前的sum和
+     * @return (0,0)位置,已知为true的位置, 倒推(0,0)位置能否到达base case位置
      */
-    private static boolean money2(int[] arr, int aim) {
+     static boolean money2(int[] arr, int aim) {
         boolean[][] dp = new boolean[arr.length + 1][aim + 1];
+        // aim列是true
         for (int i = 0; i < dp.length; i++) {
             dp[i][aim] = true;
         }
+        // 倒推填满二维表
         for (int i = arr.length - 1; i >= 0; i--) {
             for (int j = aim - 1; j >= 0; j--) {
+                // 当 j+arr[i]大于aim时，肯定是不加arr[i]的
+                dp[i][j] = dp[i+1][j];
                 if (j+arr[i] <= aim){
                     dp[i][j] = dp[i + 1][j] || dp[i + 1][j + arr[i]];
-                }else {
-                    dp[i][j] = dp[i+1][j];
                 }
             }
         }
