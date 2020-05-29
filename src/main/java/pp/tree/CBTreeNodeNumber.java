13132b2223b48a3a5b3e1083ad872c25da3e4ae2
lijingxian <lijingxian@cabrtech.com>
2020-05-29 17:34:32 +0800

    。。

--- src/main/java/pp/tree/CBTreeNodeNumber.java
+++ src/main/java/pp/tree/CBTreeNodeNumber.java
@@ -16,15 +16,22 @@ public class CBTreeNodeNumber {
         return process(head,1,getMostLeftLavel(head,1));
     }
 
+    /**
+     * @param node 当前子树的根节点
+     * @param level 根节点所在的层级
+     * @param height 树的高度
+     * @return 当前子树的节点个数
+     */
     public static int process(Node node, int level, int height){
 //        若节点所在层和树的高度相同，说明只有这一个节点
         if (level == height){
             return 1;
         }
         if (getMostLeftLavel(node.right,level+1) == height){
-//            ^ 是异或
+            // 左子树是满二叉树
             return (1 << (height-level)) + process(node.right,level+1,height);
         }else {
+            // 右子树是满二叉树
             return (1 << (height-level-1)) +process(node.left,level+1,height);
         }
     }
