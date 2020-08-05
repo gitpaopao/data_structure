43a47c73ae38c401d248ffa1f7b5e0fb0fbd929a
lijingxian <lijingxian@cabrtech.com>
2020-08-05 16:20:39 +0800

    。。

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,5 +4,10 @@
  */
 public class Test {
     public static void main(String[] args){
+        String s = "";
+        s.substring(0,s.length()-1);
+        String[] split = s.split(",");
     }
+
+
 }
